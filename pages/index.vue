4c3efebb081920aec9be3f35a2a522c4d62a873c
pattarapon <noah2302gamer@gmail.com>
2024-10-08 14:02:53 +0700

    bell

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
         <img src="/1294.jpg" alt="">
         <img src="/273508130_1545264905853283_6415174133149942349_n.jpg" alt="">
         <img src="/S__46456846.jpg" alt="">
-        <img src="/messageImage_1728358846098.jpg" alt="">
+        <img src="/27443.jpg" alt="">
         
     </div>
 </template>
