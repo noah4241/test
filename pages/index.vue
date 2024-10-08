8ae96f96ab8eae6a3756d91ec22546fc223e46a0
pattarapon <noah2302gamer@gmail.com>
2024-10-08 10:06:47 +0700

    picture

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <h1>index</h1>
-        <nuxt-link to="/team">press</nuxt-link>
+        <img src="/190818.jpg" alt="">
+        <img src="" alt="">
+        <img src="" alt="">
     </div>
 </template>
