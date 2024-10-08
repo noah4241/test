9a2baca487b9e6e2981f8425b8fccf95bef6c5e1
pattarapon <noah2302gamer@gmail.com>
2024-10-08 10:12:49 +0700

    3 picture

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
+    <div class="grid grid-cols-3">
         <img src="/190818.jpg" alt="">
-        <img src="" alt="">
-        <img src="" alt="">
+        <img src="/190817.jpg" alt="">
+        <img src="/S__4415492.jpg" alt="">
     </div>
 </template>
